6abf4fc63fc0cb0417deaffde539d7799f9a0989
Cal Crosby <calcruiseby@gmail.com>
2023-04-04 02:04:17 -0500

    made it work for binaries

--- client.py
+++ client.py
@@ -2,20 +2,73 @@ import subprocess as sub
 import sys
 from time import sleep
 
-class MagicNumber:
-    BEGIN = 250
-    TAKE_FILENAME = 251
-    END_FILENAME = 252
 
-    NOTHING = 253
-    FINISH = 254
+def encode_num(num: int):
+    """
+    Encodes a number to give information about how to ping it.
+    The number returned in the tuple is the ttl. The bool
+    returned is whether to set the DF flag.
+    """
+    if 0 <= num <= 62:
+        return (num + 65, False)
+    elif 63 <= num <= 189:
+        return (num + 65 + 1, False)
+    elif 190 <= num <= 259:
+        # I don't know why we add 3 here. It works though
+        return (num - 128 + 3, True)
+
+
+def encode_char(char: str):
+    """
+    Encodes a charactor to give information about how to ping it.
+    The number returned in the tuple is the ttl. The bool
+    returned is whether to set the DF flag.
+    """
+    return encode_num(ord(char))
+
+
+def encode_byte(byte: bytes) -> tuple[int, bool]:
+    """
+    Encodes a byte to give information about how to ping it.
+    The number returned in the tuple is the ttl. The bool
+    returned is whether to set the DF flag.
+    """
+    result = int.from_bytes(byte)
+    return encode_num(result)
+
+
+class MagicNumber:
+    """Numbers that have special meaning to the server"""
+
+    # Signals that the server should begin to interpret pings
+    BEGIN = 256
+
+    # Signals that the server should begin to take the destination filename
+    TAKE_FILENAME = 257
+
+    # Signals that the server should finish taking the destination filename
+    END_FILENAME = 258
+
+    # Signals that the transmission is over
+    FINISH = 259
 
 
 def ping_factory(host: str):
-    def ping(ttl: int):
-        args = ('ping', '-i', str(ttl), '-n', '1', host)
+    """
+    Returns a function that is used to ping the host.
+    """
+
+    def ping(ttl: int, df: bool = False):
+        """
+        Performs the ping command.
+        """
+        args = ('ping', '-i', str(ttl), '-n', '1')
+        if df:
+            args += ('-f',)
+        args += (host,)
         sub.Popen(args, stdout=sub.PIPE)
     return ping
+
 
 def main():
 
@@ -24,22 +77,24 @@ def main():
 
     ping = ping_factory(host)
 
-    ping(MagicNumber.NOTHING)
+    ping(64)
     sleep(1)
-    ping(MagicNumber.BEGIN)
+
+    ping(*encode_num(MagicNumber.BEGIN))
+    ping(*encode_num(MagicNumber.TAKE_FILENAME))
 
     for char in dest_file:
-        ping(ord(char))
+        ping(*encode_char(char))
 
-    ping(MagicNumber.END_FILENAME)
+    ping(*encode_num(MagicNumber.END_FILENAME))
 
     with open(src_file, 'rb') as f:
         byte = f.read(1)
         while byte:
-            ping(int.from_bytes(byte))
+            ping(*encode_byte(byte))
             byte = f.read(1)
 
-    ping(MagicNumber.FINISH)
+    ping(*encode_num(MagicNumber.FINISH))
 
 
 main()
--- server.py
+++ server.py
@@ -1,89 +1,156 @@
 import subprocess as sub
+from sys import byteorder
+from shutil import which
 
-class MagicNumber:
 
-    # Begin serving
-    BEGIN = 250
+def tcpdump_exists():
+    return which('tcpdump') is not None
 
-    # Begin getting destination filename
-    TAKE_FILENAME = 251
 
-    # Stop getting filename, start getting file content
-    END_FILENAME = 252
+def represent_as_word(num: int):
+    return num.to_bytes(2, byteorder)
 
-    # Do nothing
-    NOTHING = 253
 
-    # Finish transcription
-    FINISH = 254
+def ping_value_to_word(ttl: int):
+    """
+    Takes an integer value and maps it back to its value
+    as a word.
 
-    def is_magic_number(num: int):
-        return num > 127
+    128 is the default ttl for Windows, and 64 is the default
+    ttl for Linux. Therefore, we want to skip these values.
+    We also only want to listen to ttl values above 64 so that
+    each byte has a chance of making it to its destination.
 
-# Generates ICMP tcpdump lines
+    Remember that the ttl is a byte, and we use the DF flag
+    as the 9th bit.
+
+    This means that, when we do values above 255 by setting the DF
+    flag, we can't go straight to 256 because it would have a ttl of 0 since
+    the ttl is the first 8 bits.
+
+    This means that the soonest number we accept after 255 is 321.
+
+    1 0 1 0 0 0 0 0 1
+      ---------------
+           ttl
+
+    because this maintains the ttl above 64.
+    """
+
+    def to_int():
+        if 64 < ttl < 128:
+            return ttl - 65
+        elif 128 < ttl < 256:
+            return ttl - 65 - 1
+        elif 320 < ttl < 391:
+            return ttl - 65 - 1 - 65
+        else:
+            return -1
+
+    result = to_int()
+    if result < 0:
+        return None
+    return represent_as_word(result)
+
+
+class MagicWord:
+    """Words that have special meaning to our server"""
+
+    # Signals that this server is to begin interpreting pings
+    BEGIN = represent_as_word(256)
+
+    # Signals that this server is ready to receive the destination filename
+    TAKE_FILENAME = represent_as_word(257)
+
+    # Signals that this server is done receiving the destination filename
+    END_FILENAME = represent_as_word(258)
+
+    # Signals that this server is done interpreting pigns
+    FINISH = represent_as_word(259)
+
+
 def dump():
+    """Generates ICMP tcpdump lines"""
     args = ('sudo', 'tcpdump', '-vl', 'icmp', 'and', 'inbound')
     with sub.Popen(args, stdout=sub.PIPE) as tcpdump:
         for line in tcpdump.stdout:
             yield line.decode()
 
-# Parses the TTL (time to live) value from a tcpdump line
+
 def parse_ttl(tcpdump_line: str):
+    """
+    Parses the word from the given tcpdump line.
+    The ttl value represents the first 8 bits of the word,
+    and whether the DF bit is set represents the 8th bit.
+    """
+    val = 0
     lines = tcpdump_line.split(', ')
     for line in lines:
-        if line.startswith('ttl'):
-            yield int(line.split()[1])
+        if (line.startswith('ttl')):
+            val += int(line.split()[1])
+        if (line.startswith('flags')):
+            flag = line.split()[1]
+            # The DF Flag represents the 9th bit
+            if "DF" in flag:
+                val += 256
+    return -1 if not val else val
 
-# Writes a char array to a file
-def write_chars(chars: list, file: str):
-    with open(file, 'ab') as f:
-        f.write(''.join(chars).encode())
+
+def listen():
+    """Listens for ICMP traffic and generates words."""
+    for line in dump():
+        val = parse_ttl(line)
+        if val:
+            yield represent_as_word(val)
+
+
+def write_byte_list(byt: list, file_name: str):
+    """Writes a list of bytes to the given file"""
+    data = b''.join(byt)
+    with open(file_name, 'ab') as file:
+        file.write(data)
 
 
 def main():
 
-    take_filename = True
-    file_name = ""
-    chars = []
+    file_mode = True
     begun = False
+    filename = ""
+    data = []
 
-    for line in dump():
-        for ttl in parse_ttl(line):
+    if not tcpdump_exists():
+        print('Cannot find executable tcpdump. Make sure it is in your path')
+        exit(1)
 
-            # We don't begin until we receive the BEGIN magic number
-            if not begun:
-                if ttl == MagicNumber.BEGIN:
-                    begun = True
+    def flush_buffer():
+        nonlocal data, filename
+        write_byte_list(data, filename)
+
+    for word in listen():
+        if word == MagicWord.BEGIN:
+            begun = True
+        elif word == MagicWord.TAKE_FILENAME:
+            file_mode = True
+        elif word == MagicWord.END_FILENAME:
+            file_mode = False
+        elif word == MagicWord.FINISH:
+            flush_buffer()
+            begun = False
+            print('Finished writing', filename)
+        elif not begun:
+            return
+        else:
+            main_byte = word[0:1]
+            if file_mode:
+                char = main_byte.decode()
+                filename += char
+            else:
+                # We use a buffer to save on writes to the file
+                if len(data) < 4096:
+                    data.append(main_byte)
                 else:
-                    continue
-
-            if MagicNumber.is_magic_number(ttl):
-                if ttl == MagicNumber.NOTHING:
-                    continue
-
-                elif ttl == MagicNumber.TAKE_FILENAME:
-                    take_filename = True
-
-                if ttl == MagicNumber.END_FILENAME:
-                    take_filename = False
-
-                elif ttl == MagicNumber.FINISH:
-                    write_chars(chars, file_name)
-                    begun = False
-
-                continue
-
-            byte_char = chr(ttl)
-            if take_filename:
-                file_name += byte_char
-                continue
-
-            if len(chars) < 1024:
-                chars.append(byte_char)
-                continue
-            else: 
-                write_chars(chars, file_name)
-                chars = []
+                    print('writing a buffer')
+                    flush_buffer()
 
 
 main()
